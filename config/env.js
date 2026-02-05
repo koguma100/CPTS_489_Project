a9bef9dabffea13da913cd4a6e57521a2a50627c
William Fralia <118507004+koguma100@users.noreply.github.com>
2026-02-04 20:29:47 -0800

    base template

---
+++ config/env.js
@@ -0,0 +1,3 @@
+// Public Supabase configuration (SAFE for frontend)
+export const SUPABASE_URL = "https://ftoifdqoswlejdzagurp.supabase.co";
+export const SUPABASE_ANON_KEY = "";
---
+++ js/supabase.js
@@ -0,0 +1,13 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./../config/env.js";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true
+    }
+  }
+);
