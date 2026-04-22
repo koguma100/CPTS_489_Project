6b08ab4078ced189c7f0c20df01abef4b42ac8c6
William Fralia <118507004+koguma100@users.noreply.github.com>
2026-04-21 21:24:27 -0700

    changed keys to config.js for deployment

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://ftoifdqoswlejdzagurp.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0b2lmZHFvc3dsZWpkemFndXJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMzE1NDcsImV4cCI6MjA4NTgwNzU0N30.ErTa1zGyD5kvmtb9l0LlSXR-c57F3xvUNITOsPpIgxE"
+};
